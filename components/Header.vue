a9b33c4897f10f7e2a470440134571d704df0d2a
Sean Alden <sa2596491@gmail.com>
2025-06-19 10:34:36 +0700

    Revert "Deploy 125"
    This reverts commit 739081c27b044425d3df4abca1d4216928989ca1.

--- components/Header.vue
+++ components/Header.vue
@@ -314,14 +314,8 @@ onUnmounted(() => {
   emitter.off("profile-updated", fetchUser);
 });
 
-const goToEditProfile = () => {
-  isNotificationDropdownOpen.value = false;
-  router.push("/profile_pages/edit_profile_page");
-};
-const goToChangePassword = () => {
-  isNotificationDropdownOpen.value = false;
-  router.push("/profile_pages/edit_password_page");
-};
+const goToEditProfile = () => router.push("/profile_pages/edit_profile_page");
+const goToChangePassword = () => router.push("/profile_pages/edit_password_page");
 const goToNotifications = () => {
   isNotificationDropdownOpen.value = false;
   router.push("/notifications");
