d7ea6c12887bf8e532bd35fe31c1f009c68f0581
Sean Alden <sa2596491@gmail.com>
2025-06-30 23:22:31 +0700

    master4 Commit 189

--- pages/product_pages/add_product.vue
+++ pages/product_pages/add_product.vue
@@ -268,9 +268,9 @@ onMounted(() => {
       <input v-model="form.price" type="number" required class="w-full p-2 border" />
 
       <!-- Stok & Tanggal Kadaluarsa -->
-      <!-- <label>Stok & Tanggal Kadaluarsa:</label> -->
-      <!-- <label>Tanggal Kadaluarsa:</label> -->
-      <!-- <div v-for="(stock, index) in form.stocks" :key="index" class="flex gap-2 mb-2"> -->
+      <label>Stok & Tanggal Kadaluarsa:</label>
+      <label>Tanggal Kadaluarsa:</label>
+      <div v-for="(stock, index) in form.stocks" :key="index" class="flex gap-2 mb-2">
         <!-- <input
           v-model="stock.stock"
           type="number"
@@ -278,7 +278,7 @@ onMounted(() => {
           required
           class="w-1/2 p-2 border"
         /> -->
-        <!-- <input v-model="stock.exp_date" type="date" required class="w-1/2 p-2 border" />
+        <input v-model="stock.exp_date" type="date" required class="w-1/2 p-2 border" />
         <button
           type="button"
           @click="removeStockField(index)"
@@ -286,8 +286,8 @@ onMounted(() => {
           v-if="form.stocks.length > 1"
         >
           ✕
-        </button> -->
-      <!-- </div> -->
+        </button>
+      </div>
       <button
         type="button"
         @click="addStockField"
